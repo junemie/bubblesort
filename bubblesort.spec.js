aa65c1356b8ff86897c0616674f4e2839ddd4b6a
June Hosoya <junehosoya@gmail.com>
2019-01-18 17:20:50 -0500

    created 3 tests

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,10 +1,23 @@
-describe('Bubble Sort', function(){
+describe("Bubble Sort", function() {
   let swapCounter;
-    beforeEach(()=>{
-      swapCounter = 0
-    }
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+  let temp;
+  let arr = [1, 10, 8, 5, 7, 6, 3, 4, 2];
+  let arr2 = [190, 5, 6, 22, 3, 178, 200, 20];
+  beforeEach(() => {
+    swapCounter = 0;
   });
+  it("handles an empty array", function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it("expected output to be sorted", function() {
+    expect(arr).toEqual(arr.sort());
+    expect(arr2).toEqual(arr2.sort());
+  });
+  it("do not use array.prototype.sort method", function() {
+    spyOn(Array.prototype, "sort").and.callThrough();
+    expect(Array.prototype.sort.calls.count()).toBe(0);
+  });
+  if("uses the temp global variable", function(){
 
+  })
 });
